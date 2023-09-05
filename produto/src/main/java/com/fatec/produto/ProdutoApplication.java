a314743075b3bc4e5b058dea98125b8827583bb7
dti <dti@LAB204-19>
2023-09-05 09:24:57 -0300

    Refatoração Doc classe produto

--- produto/src/main/java/com/fatec/produto/ProdutoApplication.java
+++ produto/src/main/java/com/fatec/produto/ProdutoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProdutoApplication {
-
+/*
+ * Classe
+ * */
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutoApplication.class, args);
 	}
