f7d49520f260793c30df3f2fe2d7a6d10fe48527
dti <dti@10.68.76.244>
2023-09-19 11:05:35 -0300

    Ref

--- produto/src/main/java/com/fatec/produto/controller/APIProdutoController.java
+++ produto/src/main/java/com/fatec/produto/controller/APIProdutoController.java
@@ -1,4 +1,5 @@
 package com.fatec.produto.controller;
+import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -7,25 +8,38 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fatec.produto.model.Produto;
 import com.fatec.produto.servico.IProdutoServico;
 
 @RestController
-@RequestMapping("api/v1/produtos")
+@RequestMapping("/api/v1/produtos")
 public class APIProdutoController {
 	Logger logger = LogManager.getLogger(this.getClass());
 	@Autowired
 	IProdutoServico produtoServico;
-	/*
-	 * consulta catalogo retorna um arquivo json de produtos
+
+	/**
+	 * Consulta catalogo
+	 * 
+	 * @return - JSON Array com todos os produtos ou um JSON Array vazio
 	 */
-	@CrossOrigin
+	@CrossOrigin // desabilita o cors do spring security
 	@GetMapping
-	public ResponseEntity <Object> consultaTodos(){
+	public ResponseEntity<Object> consultaTodos() {
 		logger.info(">>>>>> apicontroller consulta todos");
 		return ResponseEntity.status(HttpStatus.OK).body(produtoServico.consultaCatalogo());
 	}
 
+	@CrossOrigin
+	@PostMapping
+	public ResponseEntity<Object> cadastraProduto(@RequestBody Produto p) {
+		logger.info(">>>>>> apicontroller cadastrar produto iniciado");
+		Optional<Produto> produto = produtoServico.cadastrar(p);
+		return ResponseEntity.status(HttpStatus.CREATED).body(produto.get());
+	}
 }
